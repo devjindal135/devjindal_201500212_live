6756cf21ac3142ef9d52fe3bc067fe5783371b3f
devjindal135 <109079271+devjindal135@users.noreply.github.com>
2022-07-11 23:28:33 +0530

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,8 @@
+document.querySelector(".name").innerHTML="Dev jindal";
+document.querySelector(".rollno").innerHTML="201500212";
+document.querySelector(".college").innerHTML="GLA UNIVERSITY";
+const d= new Date();
+
+var date = new Date();
+var n = date.toDateString();
+ document.querySelector(".date").innerHTML=n;
